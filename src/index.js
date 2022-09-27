c3d4cfc474fd1f555ed8c9adce211841738c981c
Alhajideen <salifunurudeen834@gmail.com>
2022-09-26 18:10:50 -0600

    set up webpack boilerplate

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './style.css';
+import './style.scss';
+console.log('Hello World!');
+
+const fancyFunc = () => {
+  return [1, 2];
+};
+
+const [a, b] = fancyFunc();
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
+  mode: 'development',
+};
